02631ea7096f24449f13474314250f2751c8cfaa
kat <34391969+kat0154@users.noreply.github.com>
2019-09-11 19:36:26 -0400

    Update regextest.js

--- commands/Misc/regextest.js
+++ commands/Misc/regextest.js
@@ -4,19 +4,21 @@ module.exports = {
      path: "../../commands/Misc/regextest.js",
      help: {
           name: "regextest",
-          alias: "regex"
+          aliases: "regex"
      },
      run: async (client,message) => {
-              		  let regex = message.content.split(" | ")[0].split(" ").slice(1);
-	      		  let match = message.content.split(" | ")[1].match(regex);
-              		  let i = 0;
+        		let regex = message.content.split(" | ")[0].split(" ").slice(1);
+	      		let match = message.content.split(" | ")[1].match(regex);
+        		let i = 0;
 			  if(match){
 					let embed = new discord.RichEmbed()
 					.setTitle("Regex Match")
 					.setColor("#00ff00")
 					.addField("Regex:",regex)
 					.addField("Input:",match.input)
-					match.slice(1,match.length).forEach(m => embed.addField(`Group ${++i}`,m))
+					match.slice(1,match.length).forEach(m => {
+            				m == undefined ? ++i : embed.addField(`Group ${++i}`,m);
+          				})
 					embed.setFooter(`by: ${message.member.displayName}`, message.author.displayAvatarURL)
 					message.channel.send(embed);
 			   } else {
